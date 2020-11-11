d06886e6a94874dab46a5f85bead131cb4a16850
Guaaan <72053761+Guaaan@users.noreply.github.com>
2020-11-11 15:18:22 -0300

    tic tac toe

---
+++ src/components/Game.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { calculateWinner } from '../helpers'
+import Board from './Board';
+
+const styles = {
+    width: '200px',
+    margin: '20px auto',
+};
+
+const Game = () => {
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [xIsNext, setXisNext] = useState(true);
+    const winner = calculateWinner(board);
+
+    const handleClick = i => {
+        const boardCopy = [...board];
+        if (winner || boardCopy[i]) return;
+        boardCopy[i] = xIsNext ? 'X' : 'O';
+        setBoard(boardCopy);
+        setXisNext(!xIsNext);
+        
+    }
+    return (
+        <>
+        <Board squares={board} onClick={handleClick} />
+        <div style={styles}>
+                <p>{winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
+        </div>
+        </>
+    )
+}
+
+export default Game;
